92c5859f726cd3e8d11fcf9efe9ab3eb983938df
SophiaS4nt <sophia.sophis@icloud.com>
2026-09-04 11:59:25 -0300

    feat: aplicando hook no index conteudo

--- my-app/src/components/Conteudo/index.tsx
+++ my-app/src/components/Conteudo/index.tsx
@@ -1,38 +1,82 @@
-import imgQuadroado from "../../img/quadrado.png";
-
+import { useState } from "react";
+import imgQuadrado from "../../img/quadrado.png";
+ 
 export default function Conteudo() {
+
+  let nomeComum: string| null = "Sophia";
+  function alterNomeComum (){
+    nomeComum = prompt ("Digite o novo nome!");
+    console.log("Nome Alterado : ", nomeComum);
+  }
+
+  const[nomeState, setNomeState]= useState<string | null>("Juquinha");
+  function alterNomeState(){
+    const nome: string | null = prompt("Digite o novo nome!");
+    setNomeState( (valorAnterior)=> valorAnterior = nome)
+    console.log("Nome Alterado : ", nomeState);
+  }
+
   return (
-    <main>
-        <section>
-            <h2>Conteúdo Principal</h2>
-            <figure>
-                <img
-          src="https://placehold.co/600x400/FFFFF0/FFFFFF/png"
-          alt="Imagem ilustrativa do conteúdo"
-            />
-            </figure>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus 
-               sapiente tempora ex dicta ullam, sunt ea sit explicabo molestias similique 
-               corrupti unde ipsam magnam, illo atque voluptates deleniti placeat quod
-               dolor veniam. Dolorum modi, aperiam numquam architecto, unde reprehenderit 
-               similique iusto iure molestias maxime corporis quam repellat sequi. Aliquam, 
-               beatae!</p>
-        </section>
-        <section>
-            <h2>Imagem interna</h2>
-            <figure>
-                <img
-          src="./quadrado-b.png"
-          alt="Imagem ilustrativa do conteúdo"
-            />
-            </figure>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus 
-               sapiente tempora ex dicta ullam, sunt ea sit explicabo molestias similique 
-               corrupti unde ipsam magnam, illo atque voluptates deleniti placeat quod
-               dolor veniam. Dolorum modi, aperiam numquam architecto, unde reprehenderit 
-               similique iusto iure molestias maxime corporis quam repellat sequi. Aliquam, 
-               beatae!</p>
-        </section>
-    </main>
-  )
+<main>
+  <div>
+    <p>Nome Comum: {nomeComum}</p>
+    <button onClick={alterNomeComum}>Nome Alterado = {nomeComum}</button>
+  </div>
+  <div>
+    <p>Nome State: {nomeState}</p>
+    <button onClick={alterNomeState}>Nome Alterado = {nomeState}</button>
+  </div>
+<section>
+<h2>Conteúdo</h2>
+<figure>
+<img
+
+            src="https://placehold.co/300x200/FF0000/FFFFFF/png"
+
+            alt="Imagem ilustrativa do conteúdo"
+
+          />
+<figcaption>
+
+            Imagem utilizada para representar o conteúdo da aplicação.
+</figcaption>
+</figure>
+<p>
+
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur
+
+          minima, numquam qui sequi ut alias, ad optio corrupti cum, pariatur
+
+          vero quod consectetur saepe veritatis explicabo obcaecati iure
+
+          eveniet est?
+</p>
+</section>
+ 
+      <section>
+<h2>Imagem Interna</h2>
+<figure>
+<img src={imgQuadrado} alt="Imagem quadrada" />
+<figcaption>
+
+            Imagem utilizada para representar o conteúdo da aplicação.
+</figcaption>
+</figure>
+<p>
+
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur
+
+          minima, numquam qui sequi ut alias, ad optio corrupti cum, pariatur
+
+          vero quod consectetur saepe veritatis explicabo obcaecati iure
+
+          eveniet est?
+</p>
+</section>
+</main>
+
+  );
+
 }
+
+ 
