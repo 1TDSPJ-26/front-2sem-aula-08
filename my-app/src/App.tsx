5bf7bdb88008dd862bbe7184dce7b2d4b3b83515
brenoell <brenobell03@gmail.com>
2026-09-04 12:01:46 -0300

    feat: conclsao da aula 09/04

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,15 +1,17 @@
 import { Outlet } from 'react-router'
 import Cabecalho from './components/Cabecalho'
 import Rodape from './components/Rodape'
+import Conteudo from './components/Conteudo'
+import Lampada from './components/Lampada'
 function App() {
- return (
- <div className="min-h-screen bg-slate-950 text-slate-100">
- <Cabecalho />
- <main className="mx-auto min-h-[76vh] max-w-5xl px-4 sm:px-6">
- <Outlet />
- </main>
- <Rodape />
- </div>
- )
+    return (
+        <div className="min-h-screen bg-slate-950 text-slate-100">
+            <Cabecalho />
+            <main className="mx-auto min-h-[76vh] max-w-5xl px-4 sm:px-6">
+                <Lampada />
+            </main>
+            <Rodape />
+        </div>
+    )
 }
 export default App
--- my-app/src/components/Conteudo/index.tsx
+++ my-app/src/components/Conteudo/index.tsx
@@ -1,8 +1,33 @@
+import { useState } from "react";
+
 export default function Conteudo() {
+  
+  let nomeComum:string| null = "Breno";
+    function alterNomeComum(){
+      nomeComum = prompt ("Digite o novo nome!");
+      console.log(prompt ("Digite o novo nome!"));
+    
+    }
+
+  useState
+  const[NomeState, setNomeState] = useState<String | null>("Breno");
+
+  function alteraNomeState(){
+    const nome: String | null = prompt("Digite o novo nome!");
+    setNomeState( (valorAnterior)=> valorAnterior = nome);
+    console.log("Nome alterado: ", NomeState);
+  }
+
   return (
     <main>
+      <div><p>Nome comum: {nomeComum}</p>
+      <button onClick={alterNomeComum}>Nome alterado = {nomeComum}</button>
       <h2>Conteúdo principal</h2>
-
+      </div>
+      <div>
+        <p>Nome State: {NomeState}</p>
+        <button onClick={alteraNomeState}>Nome alterado = {NomeState}</button>
+      </div>
       <figure>
         <img
           src="https://placehold.co/600x400/FFFFF0/FFFFFF/png"
