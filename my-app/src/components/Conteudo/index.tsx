a958cad0626565ecd6c08ace77a64be2c901c172
Lucasvieira-tech <martivieiralucas@gmail.com>
2026-09-04 12:00:51 -0300

    feat:aula sobre useState

--- my-app/src/components/Conteudo/index.tsx
+++ my-app/src/components/Conteudo/index.tsx
@@ -1,6 +1,31 @@
+import { useState } from "react";
+
 export default function Conteudo() {
+  
+ let nomeComum:string| null = "Alê";
+ function alterNomeComum(){
+  nomeComum = prompt("Díite o novo nome!");
+  console.log("Nome alterado : ", nomeComum);
+ }
+
+  const[nomeState, setNomeState] = useState<string | null>("juquinha");
+  function alteraNomeState(){
+    const nome:string | null =prompt("Dígite o novo nome!");
+    setNomeState((valorAnterior)=> valorAnterior = nome);
+    console.log("Nome alterado : ", nomeState);
+  }
+
   return (
     <main>
+      <div>
+        <p>Nome Comum : {nomeComum}</p>
+        <button onClick={alterNomeComum}>Nome ALterado = {nomeComum}</button>
+      </div>
+      <div>
+        <p>Nome State : {nomeState}</p>
+        <button onClick={alteraNomeState}>Nome ALterado = {nomeState}</button>
+
+      </div>
       <h2>Conteúdo principal</h2>
 
       <figure>
--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,6 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+
+import App from './App'
+import Home from './routes/Home'
+import Produtos from './routes/Produtos'
+import EditarProdutos from './routes/EditarProdutos'
+import Error from './routes/Error'
+import { createBrowserRouter } from 'react-router'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/produtos', element: <Produtos /> },
+      { path: '/editar-produtos', element: <EditarProdutos /> }
+    ]
+  }
+])
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
