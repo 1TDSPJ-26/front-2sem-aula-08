6abae1ed6f1c5ef204fe6f5766d8135d5001724d
dudupizzoli <edupizzolijr@gmail.com>
2026-09-09 18:44:15 -0300

    feat : fazendo o exercício da lâmpada.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,12 +2,13 @@
 import Cabecalho from './components/Cabecalho'
 import Rodape from './components/Rodape'
 import Conteudo from './components/Conteudo'
+import Lampada from './components/Lampada'
 
 export default function App() {
   return (
     <>
       <Cabecalho />
-      <Conteudo />
+      <Lampada />
       <Rodape />
     </>
   )
